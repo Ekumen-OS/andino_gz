e3c80201d48f81399d0ce06000b644e4ab166c69
Leo Neumarkt <78873247+lneumarkt@users.noreply.github.com>
2023-10-09 08:24:29 -0700

    Update Docker setup (#15)

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,71 @@
 #!/bin/bash
 
-ABSDIR=$(dirname $(readlink -f $0))
-SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Ekumen Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-docker build -t docker_humble_fortress_andino ${SCRIPTS_DIR}
+set -e
+
+# Prints information about usage.
+function show_help() {
+  echo $'\nUsage:\t build.sh [OPTIONS] \n
+  Options:\n
+  \t-i --image_name\t\t Name of the image to be built (default ros2_humble_andino_gz).\n
+  Example:\n
+  \tbuild.sh --image_name custom_image_name\n'
+}
+
+echo "Building the docker image for ros2 humble andino gazebo development."
+
+SCRIPT_FOLDER_PATH="$(cd "$(dirname "$0")"; pwd)"
+CONTEXT_FOLDER_PATH="$(cd "$(dirname "$0")"; cd .. ; pwd)"
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i|--image_name) IMAGE_NAME="${2}"; shift ;;
+        -h|--help) show_help ; exit 1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Update the arguments to default values if needed.
+OS_VERSION="focal"
+IMAGE_NAME=${IMAGE_NAME:-ros2_humble_andino_gz}
+DOCKERFILE_PATH=$SCRIPT_FOLDER_PATH/Dockerfile
+
+USERID=$(id -u)
+USER=$(whoami)
+
+sudo docker build -t $IMAGE_NAME \
+     --file $DOCKERFILE_PATH \
+     --build-arg USERID=$USERID \
+     --build-arg USER=$USER \
+     $CONTEXT_FOLDER_PATH
--- docker/run.sh
+++ docker/run.sh
@@ -1,21 +1,135 @@
 #!/bin/bash
 
-ABSDIR=$(dirname $(readlink -f $0))
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Ekumen Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-REPO_DIR=`readlink -f ${SCRIPTS_DIR}/../`
-DOCKER_MOUNT_ARGS="-v ${REPO_DIR}/:/home/ws/src"
+set +e
 
+# Prints information about usage.
+function show_help() {
+  echo $'\nUsage:\t run.sh [OPTIONS] \n
+  Options:\n
+  \t-i --image_name\t\t Name of the image to be run (default ros2_humble_andino_gz).\n
+  \t-c --container_name\t Name of the container(default ros2_humble_andino_gz_container).\n
+  \t--use_nvidia\t\t Use nvidia runtime.\n
+  Examples:\n
+  \trun.sh\n
+  \trun.sh --image_name custom_image_name --container_name custom_container_name \n'
+}
+
+# Returns true when the path is relative, false otherwise.
+#
+# Arguments
+#   $1 -> Path
+function is_relative_path() {
+  case $1 in
+    /*) return 1 ;; # false
+    *) return 0 ;;  # true
+  esac
+}
+
+echo "Running the container..."
+
+# Location of the repository
 REPOSITORY_FOLDER_PATH="$(cd "$(dirname "$0")"; cd ..; pwd)"
 REPOSITORY_FOLDER_NAME=$( basename $REPOSITORY_FOLDER_PATH )
-WORKSPACE_SRC_CONTAINER=/home/$(whoami)/ws/src/$REPOSITORY_FOLDER_NAME
 
-xhost +local:root
-docker run -it -e DISPLAY -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	${DOCKER_MOUNT_ARGS} \
-	-e "TERM=xterm-256color" \
-    -v ${REPOSITORY_FOLDER_PATH}:$WORKSPACE_SRC_CONTAINER \
-	--net=host \
-	--gpus=all -e NVIDIA_DRIVER_CAPABILITIES=all \
-    --name andino_humble_fortress \
-	--privileged --rm docker_humble_fortress_andino /bin/bash
+DSIM_REPOS_PARENT_FOLDER_PATH="$(cd "$(dirname "$0")"; cd ..; pwd)"
+# Location from where the script was executed.
+RUN_LOCATION="$(pwd)"
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i|--image_name) IMAGE_NAME="${2}"; shift ;;
+        -c|--container_name) CONTAINER_NAME="${2}"; shift ;;
+        -h|--help) show_help ; exit 1 ;;
+        --use_nvidia) NVIDIA_FLAGS="--gpus=all -e NVIDIA_DRIVER_CAPABILITIES=all" ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Update the arguments to default values if needed.
+
+IMAGE_NAME=${IMAGE_NAME:-ros2_humble_andino_gz}
+CONTAINER_NAME=${CONTAINER_NAME:-ros2_humble_andino_gz_container}
+
+SSH_PATH=/home/$USER/.ssh
+WORKSPACE_SRC_CONTAINER=/home/$(whoami)/ws/src/$REPOSITORY_FOLDER_NAME
+WORKSPACE_ROOT_CONTAINER=/home/$(whoami)/ws
+SSH_AUTH_SOCK_USER=$SSH_AUTH_SOCK
+
+# Create cache folders to store colcon build files
+mkdir -p ${REPOSITORY_FOLDER_PATH}/.build
+mkdir -p ${REPOSITORY_FOLDER_PATH}/.install
+
+# Transfer the ownership to the user
+chown -R "$USER" ${REPOSITORY_FOLDER_PATH}/.build
+chown -R "$USER" ${REPOSITORY_FOLDER_PATH}/.install
+
+# Check if name container is already taken.
+if sudo -g docker docker container ls -a | grep "${CONTAINER_NAME}$" -c &> /dev/null; then
+   printf "Error: Docker container called $CONTAINER_NAME is already opened.     \
+   \n\nTry removing the old container by doing: \n\t docker rm $CONTAINER_NAME   \
+   \nor just initialize it with a different name.\n"
+   exit 1
+fi
+
+xhost +
+sudo docker run --privileged --net=host -it $NVIDIA_FLAGS \
+       -e DISPLAY=$DISPLAY \
+       -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK_USER \
+       -v $(dirname $SSH_AUTH_SOCK_USER):$(dirname $SSH_AUTH_SOCK_USER) \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v ${REPOSITORY_FOLDER_PATH}:$WORKSPACE_SRC_CONTAINER \
+       -v ${REPOSITORY_FOLDER_PATH}/.build:$WORKSPACE_ROOT_CONTAINER/build:rw \
+       -v ${REPOSITORY_FOLDER_PATH}/.install:$WORKSPACE_ROOT_CONTAINER/install:rw \
+       -v $SSH_PATH:$SSH_PATH \
+       --name $CONTAINER_NAME $IMAGE_NAME
+xhost -
+
+# Trap workspace exits and give the user the choice to save changes.
+function onexit() {
+  while true; do
+    read -p "Do you want to overwrite the image called '$IMAGE_NAME' with the current changes? [y/n]: " answer
+    if [[ "${answer:0:1}" =~ y|Y ]]; then
+      echo "Overwriting docker image..."
+      sudo docker commit $CONTAINER_NAME $IMAGE_NAME
+      break
+    elif [[ "${answer:0:1}" =~ n|N ]]; then
+      break
+    fi
+  done
+  docker stop $CONTAINER_NAME > /dev/null
+  docker rm $CONTAINER_NAME > /dev/null
+}
+
+trap onexit EXIT
